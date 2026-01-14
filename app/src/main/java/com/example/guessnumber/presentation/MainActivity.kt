6df004190163ffc7152a749430061068133cf945
Slavikus1 <vyacheslav921@icloud.com>
2026-01-14 18:29:04 +0300

    jetpack nav and safeArgs

--- app/src/main/java/com/example/guessnumber/presentation/MainActivity.kt
+++ app/src/main/java/com/example/guessnumber/presentation/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.guessnumber.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.example.guessnumber.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(
+                left = systemBars.left,
+                top = systemBars.top,
+                right = systemBars.right,
+                bottom = systemBars.bottom
+            )
+            insets
+        }
     }
 }
